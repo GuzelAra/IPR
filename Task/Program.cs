07432ee4fc9c02dcffdfecca24846d54399b3377
Guzel Elanskaya <guzelarazova@yandex.ru>
2023-01-03 21:03:54 +0300

    added Task

---
+++ Task/Program.cs
@@ -0,0 +1,23 @@
+﻿
+
+string[] arr1 = new string[5] {"hello", "my", "friend", "Mia", "!"};
+string[] arr2 = new string[arr1.Length];
+void Array2 (string[] arr1, string[] arr2)
+{
+int count = 0;
+for (int i = 0; i < arr1.Length; i++)
+    {
+    if(arr1[i].Length <= 3)
+        {
+        arr2[i] = arr1[i];
+        count++;
+        }
+    }
+}
+
+Array2 (arr1, arr2);
+for (int i = 0; i < arr2.Length; i++)
+    {
+        Console.Write($"{arr2[i]} ");
+    }
+    Console.WriteLine();
